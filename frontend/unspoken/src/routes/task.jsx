1994a1d6bd452d22bdeb4e338e58818ae15c0316
Vlad Radchenko <vradchenko@callpanda.com>
2023-11-26 03:12:30 +0300

    Improvements

--- frontend/unspoken/src/routes/task.jsx
+++ frontend/unspoken/src/routes/task.jsx
@@ -1,65 +1,66 @@
-import React, { useState, useEffect } from "react";
-import {
-  useParams,
-  useNavigate,
-  useLoaderData,
-  useNavigation,
-} from "react-router-dom";
-import { Chip, Spacer, Spinner } from "@nextui-org/react";
+import React, {useEffect, useState} from "react";
+import {useLoaderData, useNavigation, useParams,} from "react-router-dom";
+import {Chip, Spacer, Spinner} from "@nextui-org/react";
 import StatusChip from "../components/status";
 import Message from "../components/message";
 import "../index.css";
 
-function Tasks(props) {
+const fetchData = async (id) => {
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/task/${id}/`);
+  return await response.json();
+};
+
+function Tasks() {
   const { id } = useParams();
-  const loadedData = useLoaderData();
-  const { data } = loadedData;
+  const { data } = useLoaderData();
   const { state } = useNavigation();
+  const [task, setTask] = useState(data);
+
+  function isFinal(task) {
+    return task.status === "completed" || task.status === "failed";
+  }
 
   useEffect(() => {
-    console.log(loadedData);
+    let interval;
+    if (!isFinal(task)) {
+      interval = setInterval(async () => {
+        const fetchedData = await fetchData(id);
+        setTask(fetchedData);
+        if (isFinal(fetchedData)) {
+          clearInterval(interval);
+        }
+      }, 5000);
+
+      return () => clearInterval(interval);
+    }
   }, [id]);
 
   if (state === "loading") {
     return <Spinner color="secondary" size="lg" />;
   }
 
-  if (data?.status !== "completed") {
-    console.log("data:", data);
-    return (
-      <div className="flex flex-row">
-        <div className="basis-1/6">
-          <Chip radius="sm">id: {data?.id}</Chip>
-        </div>
-        <div className="basis-1/6">
-          <StatusChip status={data?.status} />
-        </div>
-        <div className="basis-2/3">
-          <p className="font-mono line-clamp-1">{data?.file_name}</p>
-        </div>
-      </div>
-    );
-  }
   return (
     <div>
       <div className="flex flex-row">
         <div className="basis-1/6">
-          <Chip radius="sm">id: {data.id}</Chip>
+          <Chip radius="sm">id: {task.id}</Chip>
         </div>
         <div className="basis-1/6">
-          <StatusChip status={data.status} />
+          <StatusChip status={task.status} />
         </div>
         <div className="basis-2/3">
-          <p className="font-mono line-clamp-1">{data.file_name}</p>
+          <p className="font-mono line-clamp-1">{task?.file_name}</p>
         </div>
       </div>
       <Spacer y={3} />
 
-      <div>
-        {data.messages.map((message, i) => (
-          <Message key={i} text={message.text} speaker={message.speaker} />
-        ))}
-      </div>
+      {task.status === "completed" && (
+        <div>
+          {task.messages.map((message, i) => (
+            <Message key={i} text={message.text} speaker={message.speaker} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
