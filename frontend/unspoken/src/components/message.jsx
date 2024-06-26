410b959a429671dc68e538c450e2b450e3d914ad
radchenko.v <radchenko.v@selectel.ru>
2024-06-26 16:38:12 +0300

    Improve message component

--- frontend/unspoken/src/components/message.jsx
+++ frontend/unspoken/src/components/message.jsx
@@ -1,33 +1,26 @@
 import { Card, CardBody, CardHeader } from "@nextui-org/react";
 
 export default function Message(props) {
-  const { speaker, text , start} = props;
-  console.log('start', start)
-  const speakerStyles = {
-    speaker_0: "message-violet",
-    speaker_1: "message-blue",
-    speaker_2: "message-indigo",
-    speaker_3: "message-purple",
-    speaker_4: "message-pink",
-    unknown: "message-rose",
-  };
+  const { speaker, text, start } = props;
 
   const toTimeStamp = (time) => {
-    console.log(time)
     var date = new Date(0);
     date.setSeconds(Math.trunc(time));
     return date.toISOString().substring(11, 19);
   };
 
-  const messageStyle = speakerStyles[speaker] || speakerStyles.unknown;
 
   return (
     <div className="flex justify-start">
-      <div>{start ? toTimeStamp(start): '00:00:00'}</div>
       <div>
         <Card className="message flat" variant="flat">
           <CardHeader className="justify-between" variant="flat">
             <div className="flex gap-4">
+              <div className="flex  items-start justify-center uncopy">
+                <h5 className="text-small tracking-tight text-default-400">
+                  {start ? toTimeStamp(start) : '00:00:00'}
+                </h5>
+              </div>
               <div className="flex  items-start justify-center uncopy">
                 <h5 className="text-small tracking-tight text-default-400">
                   @{speaker}
@@ -37,7 +30,7 @@ export default function Message(props) {
           </CardHeader>
           <CardBody variant="flat">
             <p className="font-mono">
-              <span className="hidden-text">{speaker}: </span>
+              <span className="hidden-text">{start ? toTimeStamp(start) : '00:00:00'} - {speaker}: </span>
               {text}
             </p>
           </CardBody>
