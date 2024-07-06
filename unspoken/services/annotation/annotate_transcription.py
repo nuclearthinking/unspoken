4eeefca14b4243d8927894fbc36310ac87dee25f
Vlad Radchenko <vradchenko@callpanda.com>
2024-07-06 21:42:41 +0300

    Change determine speaker logic from hit count to overlap duration. Removed outdated tasks.

--- unspoken/services/annotation/annotate_transcription.py
+++ unspoken/services/annotation/annotate_transcription.py
@@ -9,7 +9,43 @@ from unspoken.enitites.speach_to_text import SpeachToTextResult
 logger = logging.getLogger(__name__)
 
 
-def _get_who_speaking(start: float, end: float, segments: list[SpeakerSegment]) -> str:
+def _determine_speaker_by_time(start: float, end: float, segments: list[SpeakerSegment]) -> str:
+    """
+    Determine the speaker based on the start and end timestamps and the given segments.
+
+    :param start: The start timestamp.
+    :param end: The end timestamp.
+    :param segments: A list of SpeakerSegment objects.
+    :return: The speaker ID or 'unknown' if no speaker is found.
+    """
+    speaker_counter = defaultdict(float)
+    for segment in segments:
+        # Calculate overlap between transcription segment and speaker segment
+        overlap_start = max(start, segment.start)
+        overlap_end = min(end, segment.end)
+        if overlap_start < overlap_end:  # There is an overlap
+            overlap_duration = overlap_end - overlap_start
+            speaker_counter[segment.speaker] += overlap_duration
+
+    if not speaker_counter:
+        logger.info('No speaker speaking')
+        return 'unknown'
+
+    logger.debug('Speaker counter now %s', speaker_counter)
+    speaker = max(speaker_counter.items(), key=itemgetter(1))[0]
+    logger.debug('Speaker %s speaking', speaker)
+    return speaker
+
+
+def _determine_speaker_by_hit_count(start: float, end: float, segments: list[SpeakerSegment]) -> str:
+    """
+    Determine the speaker based on the start and end timestamps and the given segments.
+
+    :param start: The start timestamp.
+    :param end: The end timestamp.
+    :param segments: A list of SpeakerSegment objects.
+    :return: The speaker ID or 'unknown' if no speaker is found.
+    """
     speaker_counter = defaultdict(float)
     hit_segments = []
     for segment in segments:
@@ -28,11 +64,18 @@ def _get_who_speaking(start: float, end: float, segments: list[SpeakerSegment]) 
 
 
 def annotate(stt_result: SpeachToTextResult, diarization_result: DiarizationResult) -> TranscriptionResult:
+    """
+    Annotate the speech-to-text result with speaker information from the diarization result.
+
+    :param stt_result: The SpeachToTextResult object containing the speech-to-text segments.
+    :param diarization_result: The DiarizationResult object containing the speaker diarization segments.
+    :return: A TranscriptionResult object with annotated speaker information.
+    """
     result = TranscriptionResult()
     stt_segments = stt_result.segments
     diarization_segments = diarization_result.segments
     for segment in stt_segments:
-        speaker = _get_who_speaking(start=segment.start, end=segment.end, segments=diarization_segments)
+        speaker = _determine_speaker_by_time(start=segment.start, end=segment.end, segments=diarization_segments)
         result.messages.append(
             TranscriptionSegment(
                 speaker=speaker,
--- unspoken/services/queue/tasks/__init__.py
+++ unspoken/services/queue/tasks/__init__.py
@@ -1,5 +1,1 @@
 from .transcribe_task import transcribe
-from .convert_audio_task import convert_audio
-from .diarize_audio_task import diarize_audio
-from .transcribe_audio_task import speach_to_text
-from .complete_transcribing_task import complete_transcription
