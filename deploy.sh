d7df3c3db604478980a11d07732060f3b8846989
nuclearthinking <onifent@gmail.com>
2024-07-11 19:45:36 +0300

    Easy deploy (#8)
    Added deploy script. 
    Removed singleton meta classes for heavy services like transcriber & diarizer.
    Changed amount of parrallel workers to 2 instead of 4 due to CUDA OOM errors.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-$IP_ADDRESS = $(hostname -I | awk '{print $1}')
+IP_ADDRESS=$(hostname -I | awk '{print $1}')
 echo "Host address: $IP_ADDRESS"
-echo "Building frontend with api backed address as $IP_ADDRESS."
+echo "Building frontend with api backed address as: $IP_ADDRESS."
 VITE_API_HOST=$IP_ADDRESS docker compose build frontend
 echo "Running transcribator components containers."
 docker compose --profile extra --profile frontend up -d
