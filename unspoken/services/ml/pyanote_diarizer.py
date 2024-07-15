0a1de66eceb225d2b88906d6f9a8ca6f93387060
radchenko.v <radchenko.v@selectel.ru>
2024-07-15 13:23:09 +0300

    Disable torch gradient calculation while transcribing and diarization.

--- unspoken/services/ml/pyanote_diarizer.py
+++ unspoken/services/ml/pyanote_diarizer.py
@@ -20,7 +20,8 @@ class PyanoteDiarizer(BaseDiarizer):
         with NamedTemporaryFile(suffix='.wav') as fp:
             fp.write(wav_data)
             fp.flush()
-            diarization = self._pipeline(fp.name)
+            with torch.no_grad():
+                diarization = self._pipeline(fp.name)
 
         result = DiarizationResult()
         speakers = set()
