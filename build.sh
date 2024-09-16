76c9ad0b645ebc9f037dd45fca1ae796d3d27e67
radchenko.v <radchenko.v@selectel.ru>
2024-09-16 17:09:16 +0300

    ci: added build script, target frontend to latest version of image

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+if [ $# -eq 0 ]; then
+    echo "Error: Please provide a version number."
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+
+VERSION=$1
+
+docker build --platform linux/amd64 -t gitlab-registry.selectel.org/radchenko.v/unspoken:${VERSION} -f frontend.Dockerfile .
+docker build --platform linux/amd64 -t gitlab-registry.selectel.org/radchenko.v/unspoken:latest -f frontend.Dockerfile .
+docker push gitlab-registry.selectel.org/radchenko.v/unspoken:${VERSION}
+docker push gitlab-registry.selectel.org/radchenko.v/unspoken:latest
